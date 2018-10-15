5d7ca3b6d371cf2524b366b23afa69f3302f868c
TaylorFrazier <41766869+TaylorFrazier@users.noreply.github.com>
2018-10-14 23:26:16 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-wget https://github.com/Emrals/emrals/releases/download/v0.12.2.4.2/linux-x64.tar.gz
+wget https://github.com/Emrals/emrals/releases/download/v0.12.2.5.2/linux-x64.tar.gz
 tar xzfv linux-x64.tar.gz
-cp -r ~/.emralscore ~/.emralscore-bak
-cp /usr/local/bin/emralsd emralsd-bak
+cp -r ~/.emralscore ~/.emralscore-bak-v0.12.2.5.2
+cp /usr/local/bin/emralsd emralsd-bak-v0.12.2.5.2
 killall emralsd
 sudo cp emralsd /usr/local/bin/emralsd
+sudo cp emrals-cli /usr/local/bin/emrals-cli
 emralsd -reindex
