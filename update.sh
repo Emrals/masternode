1548714f2ac6c476a38a3b2cc9f78c87363d76e5
TaylorFrazier <41766869+TaylorFrazier@users.noreply.github.com>
2018-10-15 21:27:56 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 systemctl stop EMRALS.service
+sleep 10
 wget https://github.com/Emrals/emrals/releases/download/v0.12.2.5.2/linux-x64.tar.gz
 tar xzfv linux-x64.tar.gz
 cp -r ~/.emralscore ~/.emralscore-bak-v0.12.2.5.2
@@ -9,6 +10,7 @@ killall emralsd
 sudo cp emralsd /usr/local/bin/emralsd
 sudo cp emrals-cli /usr/local/bin/emrals-cli
 systemctl start EMRALS.service
+sleep 10
 if ! pgrep -x "emralsd" > /dev/null
 then
     emralsd
