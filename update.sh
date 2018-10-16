43c1dd5d09c0985382407abbad6be304882728c2
TaylorFrazier <41766869+TaylorFrazier@users.noreply.github.com>
2018-10-15 21:22:48 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+systemctl stop EMRALS.service
 wget https://github.com/Emrals/emrals/releases/download/v0.12.2.5.2/linux-x64.tar.gz
 tar xzfv linux-x64.tar.gz
 cp -r ~/.emralscore ~/.emralscore-bak-v0.12.2.5.2
 cp /usr/local/bin/emralsd emralsd-bak-v0.12.2.5.2
+cp /usr/local/bin/emrals-cli emrals-cli-bak-v0.12.2.5.2
 killall emralsd
 sudo cp emralsd /usr/local/bin/emralsd
 sudo cp emrals-cli /usr/local/bin/emrals-cli
 #emralsd -reindex
+systemctl start EMRALS.service
 emralsd
